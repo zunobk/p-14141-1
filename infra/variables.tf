2018bb9288ed79ec282525cf303fed13470f6889
zunobk <junho4560@naver.com>
2026-03-10 12:35:17 +0900

    work

--- infra/variables.tf
+++ infra/variables.tf
@@ -5,10 +5,10 @@ variable "region" {
 
 variable "prefix" {
   description = "Prefix for all resources"
-  default     = "blog"
+  default     = "shop"
 }
 
 variable "app_1_domain" {
   description = "app_1 domain"
-  default     = "api.blog.jhs512.site"
+  default     = "api.shop.zunobk.site"
 }
